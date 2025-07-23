747453583f00f2da611972f2b4644f6888ab28af
Nilesh Khodiyar <nileshkhodiyar2000@gmail.com>
2025-07-23 10:42:09 +0000

    1. Config file is written acc. to .env file 2. Replaced hardcoded env. of main.go with our config struct.

--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import (
 	"database/sql"
 	"log"
 
-    _"github.com/lib/pq"
+	_ "github.com/lib/pq"
 	"github.com/nilesh0729/OrdinaryBank/api"
 	Anuskh "github.com/nilesh0729/OrdinaryBank/db/Result"
+	"github.com/nilesh0729/OrdinaryBank/util"
 )
 
-const(
-	dbDriver = "postgres"
-	dbSource = "postgres://root:SituBen@localhost:5433/Hiten?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main(){
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot Load Config", err)
+	}
+	
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot Connect To db: ", err)
 	}
@@ -26,7 +27,7 @@ func main(){
 	server := api.NewServer(store)
 
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil{
 		log.Fatal("Cannot Start Server : ", err)
 	}
